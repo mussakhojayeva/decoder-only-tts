fixed issue with masking in multi-gpu setting

load_dataset.py
@@ -11,7 +11,7 @@ def collate_fn(batch):
     text_lengths, ids_sorted_decreasing = torch.sort(
     torch.LongTensor([len(x[0]) for x in batch]),
     dim=0, descending=True)
-    max_text_len = text_lengths[0]
+    max_text_len = text_lengths[0].item()
     
     text_padded = torch.zeros(len(batch), max_text_len, dtype=torch.long)
     for i in range(len(ids_sorted_decreasing)):
@@ -19,19 +19,19 @@ def collate_fn(batch):
         text_padded[i, :text.size(0)] = text
         
     num_mels = batch[0][1].size(1)
-    max_mel_length = max([x[1].size(0) for x in batch])
+    max_mel_len = max([x[1].size(0) for x in batch])
     
-    mel_padded = torch.zeros(len(batch), max_mel_length, num_mels)
-    stop_padded = torch.zeros(len(batch), max_mel_length)
+    mel_padded = torch.zeros(len(batch), max_mel_len, num_mels)
+    stop_padded = torch.zeros(len(batch), max_mel_len)
     mel_lengths = torch.LongTensor(len(batch))
-    
+        
     for i in range(len(ids_sorted_decreasing)):
         mel = batch[ids_sorted_decreasing[i]][1]
         mel_padded[i, :mel.size(0), :] = mel
         stop_padded[i, mel.size(0)-1:] = 1
         mel_lengths[i] = mel.size(0)
      
-    return text_padded, text_lengths, mel_padded, mel_lengths, stop_padded
+    return text_padded, text_lengths, mel_padded, mel_lengths, stop_padded, max_text_len, max_mel_len
 
 
 class PrepareDataset(Dataset):

model.py
@@ -89,7 +89,9 @@ class DecoderTTS(nn.Module):
         text: torch.Tensor,
         mel: torch.Tensor,
         text_lengths: torch.Tensor,
-        mel_lengths: torch.Tensor
+        mel_lengths: torch.Tensor,
+        max_text_len: int,
+        max_mel_len: int
         
     ):
         #text = text[:,:text_lengths.max().item()]
@@ -100,10 +102,9 @@ class DecoderTTS(nn.Module):
         tokens = self.text_emb(text)
         tokens += self.text_pos_emb(tokens)
         text_seq_len = tokens.shape[1] #+ 1 ## one for <bos>
-        set_trace()
         
-        text_mask = get_mask_from_lengths(text_lengths)
-        mel_mask = get_mask_from_lengths(mel_lengths)
+        text_mask = get_mask_from_lengths(text_lengths, max_text_len)
+        mel_mask = get_mask_from_lengths(mel_lengths, max_mel_len)
 
         masks = torch.cat((text_mask, mel_mask), dim = 1)
         if mel.nelement() > 0:

trainer.py
@@ -26,10 +26,10 @@ def train_fn(model, dataloader, optimizer):
     model.train()
     for num, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
         optimizer.zero_grad()
-        text_padded, text_lengths, mel_padded, mel_lengths, end_logits_padded = [
-            x.cuda() for x in batch
-        ]        
-        mel_pred, end_logits_pred = model(text_padded, mel_padded, text_lengths, mel_lengths)
+        text_padded, text_lengths, mel_padded, mel_lengths, end_logits_padded, max_text_len, max_mel_len = [
+            x.cuda() if type(x) != int else x for x in batch
+        ]
+        mel_pred, end_logits_pred = model(text_padded, mel_padded, text_lengths, mel_lengths, max_text_len, max_mel_len)
         loss = loss_fn(mel_pred, mel_padded, end_logits_pred, end_logits_padded, mel_lengths)
         running_loss += loss.sum().item()
         loss.sum().backward()
@@ -44,10 +44,10 @@ def eval_fn(model, dataloader):
     model.eval()
     with torch.no_grad():
         for num, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
-            text_padded, text_lengths, mel_padded, mel_lengths, end_logits_padded = [
-                x.cuda() for x in batch
-            ]        
-            mel_pred, end_logits_pred = model(text_padded, mel_padded, text_lengths, mel_lengths)
+            text_padded, text_lengths, mel_padded, mel_lengths, end_logits_padded, max_text_len, max_mel_len = [
+                x.cuda() if type(x) != int else x for x in batch
+            ]
+            mel_pred, end_logits_pred = model(text_padded, mel_padded, text_lengths, mel_lengths, max_text_len, max_mel_len)
             loss = loss_fn(mel_pred, mel_padded, end_logits_pred, end_logits_padded, mel_lengths)
             running_loss += loss.item()
         